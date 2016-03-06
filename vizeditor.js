ab05665a3e928d342b95d671d03e672b5a9dd8c6
hqcasanova <hqcasanova@users.noreply.github.com>
2016-03-06 21:14:23 +0000

    Adds free text capabilites, uses IE8-compatible element searches, adds language selection, adds delete button and beautifies interface

--- vizeditor.js
+++ vizeditor.js
@@ -1,5 +1,10 @@
 (function () {
-    var imgCard;
+    'use strict';
+
+    var langSelEl;
+    var newCardEl;
+    var textAreaEl;
+    var imgCardEl;
 
     //Polyfill for IE8
     if (!String.prototype.trim) {
@@ -8,18 +13,64 @@
         };
     }
 
-    //Set focus on first and only present text area
-    document.getElementsByClassName('caption')[0].focus();
+    //Needed for referencing the chosen language on keypress (see 'onWordBoundary' below)
+    langSelEl = document.getElementById('select-language');
 
-    imgCard = document.getElementsByClassName('image-card')[0].cloneNode(true);
+    //Caches block of free text, the image cards' container
+    textAreaEl = document.getElementById('textarea');
 
-    //Handler for new word search on pressing tab, space or enter
-    document.getElementById('textarea').onkeydown = function (event) {
+    //Sets up the 'mold' for new image cards using the current (and only) image card
+    imgCardEl = textAreaEl.children[0]
+    newCardEl = imgCardEl.cloneNode(true);
+
+    //Makes cursor appear on first image card's input area below the pictogram
+    imgCardEl.children[1].focus();
+
+    //Assigns global handlers
+    textAreaEl.onkeydown = onWordBoundary;
+    document.getElementById('delete-button').onclick = onDelete;
+
+
+    //Wipes out all the existing image cards and adds a blank one.
+    function onDelete () {
+        textAreaEl.innerHTML = newCardEl.outerHTML;
+        textAreaEl.children[0].children[1].focus();
+    }
+
+    function onPictoResponse (word, imgUrls) {
+        var imgEl;
+
+        if (imgUrls.length) {
+            imgEl = this.children[0];
+            imgEl.src = imgUrls[0];
+            imgEl.alt = word;
+        }
+    }
+
+    function onWordBoundary (event) {
         event = event || window.event;
 
-        if ((event.target && event.target.className == 'caption' ) && (event.keyCode == 13 || event.keyCode == 9 || event.keyCode == 32)) {
+        var target = event.target;
+        var word;
+
+        if ((target && target.className == 'caption' ) && (event.keyCode == 13 || event.keyCode == 9 || event.keyCode == 32)) {
+            
+            //Prevent any caret movements or further changes to the text
             event.preventDefault();
-            Vizpedia.getUrls(event.target.innerHTML.trim());
+            target.contentEditable = 'false';
+
+            //Fetches the urls of all images corresponding to the term just typed in
+            word = event.target.innerHTML.trim();
+            window.Vizpedia.getUrls(
+                word, 
+                langSelEl.value, 
+                onPictoResponse.bind(target.parentElement.children[0], word)
+            );
+
+            //Appends a new empty image card right next to the current one and makes caret appear on it
+            textAreaEl.appendChild(newCardEl);
+            newCardEl.children[1].focus();
+            newCardEl = newCardEl.cloneNode(true);
         }
-    };
+    }
 }());
