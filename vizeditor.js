b5496dbe49a1d7464d5bc559d1b52e849c762c6b
hqcasanova <hqcasanova@users.noreply.github.com>
2016-03-06 14:09:28 +0000

    Adds trim polyfill and prevents default action on word boundary

--- vizeditor.js
+++ vizeditor.js
@@ -1,16 +1,26 @@
 (function () {
-    var imgCard = document.getElementsByClassName('image-card')[0].cloneNode(true);
+    var imgCard;
 
+    //Polyfill for IE8
+    if (!String.prototype.trim) {
+        String.prototype.trim = function () {
+            return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+        };
+    }
+
+    //Set focus on first and only present text area
     document.getElementsByClassName('caption')[0].focus();
+
+    imgCard = document.getElementsByClassName('image-card')[0].cloneNode(true);
 
     //Handler for new word search on pressing tab, space or enter
     document.getElementById('textarea').onkeydown = function (event) {
         event = event || window.event;
 
+        event.preventDefault();
+
         if ((event.target && event.target.className == 'caption' ) && (event.keyCode == 13 || event.keyCode == 9 || event.keyCode == 32)) {
-            Vizpedia.getUrls(event.target.innerHTML);
-        } else {
-            console.log(event);
-      }
+            Vizpedia.getUrls(event.target.innerHTML.trim());
+        }
     };
 }());
